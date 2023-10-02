85d69b399f070de8733df6d604f65b3a630a65d6
ZequnZ <zequn.zhou007@gmail.com>
2023-10-02 14:58:20 +0200

    update test lambda

--- Localstack/lambda/test_lambda.py
+++ Localstack/lambda/test_lambda.py
@@ -1,16 +1,22 @@
-import basic_lambda_testutils as main_utils
-import unittest
-unittest.TestLoader.sortTestMethodsUsing = None
+import basic_lambda_testutils as testutils
+from unittest import TestCase
+import time
+
 lambda_func_name = 'basic_lambda'
 
-class Test(unittest.TestCase):
-    def test_a_setup_class(self):
-        print('\r\nCreating the lambda function...')
-        main_utils.create_lambda(lambda_func_name)
-    def test_b_invoke_function_and_response(self):
-        print('\r\nInvoking the lambda function...')
-        payload = main_utils.invoke_function(lambda_func_name)
-        self.assertEqual(payload['message'], 'Hello User!')
-    def test_c_teardown_class(self):
-        print('\r\nDeleting the lambda function...')
-        main_utils.delete_lambda(lambda_func_name)
+class Test(TestCase):
+
+    @classmethod
+    def setup_class(cls):
+        print('\r\nSetting up the class')
+        testutils.create_lambda(lambda_func_name)
+        time.sleep(20)
+
+    @classmethod
+    def teardown_class(cls):
+        print('\r\nTearing down the class')
+        testutils.delete_lambda(lambda_func_name)
+
+    def test_that_lambda_returns_correct_message(self):
+        payload = testutils.invoke_function(lambda_func_name)
+        self.assertEqual(payload['message'], 'Hello Mei Zhou!')
